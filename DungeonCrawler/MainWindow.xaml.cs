45cd6b39d0fbaea5d88cb29c9e1ef72c2c42fddb
David Mulligan <dvdmlgn@outlook.com>
2018-01-30 20:48:17 +0000

    can draw 'player' rectangle to canvas, can move player with 'WASD' keys

--- DungeonCrawler/MainWindow.xaml.cs
+++ DungeonCrawler/MainWindow.xaml.cs
@@ -20,9 +20,55 @@ namespace DungeonCrawler
     /// </summary>
     public partial class MainWindow : Window
     {
+        Rectangle player = new Rectangle();
+
+        public static int x = 0;
+        public static int y = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            player.Height = 20;
+            player.Width = 20;
+
+            player.Fill = Brushes.ForestGreen;
+
+            canvas.Children.Add(player);
+
+            Canvas.SetLeft(player, x);
+            Canvas.SetTop(player, y);
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (Keyboard.IsKeyDown(Key.A))
+            {
+                x -= 7;
+
+                Canvas.SetLeft(player, x);
+            }
+
+            if (Keyboard.IsKeyDown(Key.D))
+            {
+                x += 7;
+
+                Canvas.SetLeft(player, x);
+            }
+
+            if (Keyboard.IsKeyDown(Key.W))
+            {
+                y -= 7;
+
+                Canvas.SetTop(player, y);
+            }
+
+            if (Keyboard.IsKeyDown(Key.S))
+            {
+                y += 7;
+
+                Canvas.SetTop(player, y);
+            }
         }
     }
 }
