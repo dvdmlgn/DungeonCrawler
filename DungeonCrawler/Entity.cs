b1cd00f99232c1b66a06033a5b55bf91b2877716
David Mulligan <dvdmlgn@outlook.com>
2018-02-01 11:15:48 +0000

    tiding up and re-organising again

--- DungeonCrawler/Entity.cs
+++ DungeonCrawler/Entity.cs
@@ -10,24 +10,66 @@ namespace DungeonCrawler
 {
     public class Entity
     {
+        #region Properties
+      // ---------------------------------------------------------------------------------------------------------
         public int X { get; set; }
         public int Y { get; set; }
 
         public int Speed { get; set; }
 
-        public Rectangle CollisionMask;
+        public Rectangle CollisionMask = new Rectangle();
+      // ---------------------------------------------------------------------------------------------------------
+        #endregion
 
-        public Entity()
+        #region Constructors
+      // ---------------------------------------------------------------------------------------------------------
+        public Entity(EntityStats stats)
         {
-            CollisionMask = new Rectangle();
-
-            
+            SetStats(stats);
         }
 
-        public Entity(Canvas canvas)
+        public Entity(EntityStats stats, Canvas canvas)
         {
+            SetStats(stats);
 
+            AddToCanvas(canvas);
         }
+
+
+        private void SetStats(EntityStats stats)
+        {
+            this.X = stats.X;
+            this.Y = stats.Y;
+
+            this.Speed = stats.Speed;
+
+            this.CollisionMask = stats.CollisionMask;
+        }
+
+        private void AddToCanvas(Canvas canvas)
+        {
+            canvas.Children.Add(this.CollisionMask);
+
+            Canvas.SetLeft(this.CollisionMask, this.X);
+            Canvas.SetTop(this.CollisionMask, this.Y);
+        }
+      // ---------------------------------------------------------------------------------------------------------
+        #endregion
+
+        #region Methods
+      // ---------------------------------------------------------------------------------------------------------
+        public void Move(int x, int y)
+        {
+          // update Entites Co-Ordinates with new positions
+            this.X += x;
+            this.Y += y;
+
+          // apply Entites new position to the Canvas
+            Canvas.SetLeft(this.CollisionMask, this.X);
+            Canvas.SetTop(this.CollisionMask, this.Y);
+        }
+      // ---------------------------------------------------------------------------------------------------------
+        #endregion
 
     }
 }
---
+++ DungeonCrawler/EntityStats.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+
+namespace DungeonCrawler
+{
+    public struct EntityStats
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public int Speed { get; set; }
+
+        public Rectangle CollisionMask { get; set; }
+    }
+}
